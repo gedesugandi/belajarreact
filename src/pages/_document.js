0318982a3302ba95f377182041cd6f1b4218398d
gedesugandi <gedesugandi@gmail.com>
2025-07-09 21:55:37 +0800

    update : add meta google search console

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="kcBgTZRg3mp333Duor8iVzJ3meMpkdzzFfX_PZuWkic" />
       </Head>
       <body className="antialiased min-h-screen bg-gray-50 text-gray-800">
         <Main />
