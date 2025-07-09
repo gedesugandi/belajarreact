a2b627be76c2a78598feff2a4e06d836e710f0d3
gedesugandi <gedesugandi@gmail.com>
2025-07-09 16:56:14 +0800

    update heading home

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <div className='py-10'>
-        <h1 className="text-2xl font-semibold mb-4">Selamat datang di Tools Website</h1>
+        <h1 className="text-2xl font-semibold mb-4">Selamat datang di Tools BedahDigital</h1>
         <ul className="space-y-2">
           <li>
             <Link href="/cloaking-checker" className="text-blue-600 hover:underline">
