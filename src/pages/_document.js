df2e6f4ceea1d5ecccaac202d8b17bcea155c8ea
gedesugandi <gedesugandi@gmail.com>
2025-07-09 21:52:11 +0800

    update add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="antialiased min-h-screen bg-gray-50 text-gray-800">
         <Main />
         <NextScript />
